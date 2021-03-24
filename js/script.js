927bee03826ee971567ab1b0363c9fa9fbd6276e
giselearaujos <giselearaujo07.s@gmail.com>
2021-03-24 13:57:05 -0300

    � Create javascript.

---
+++ js/script.js
@@ -0,0 +1,17 @@
+let img = document.getElementById("img")
+
+function btn1(){
+    img.src = "https://media.giphy.com/media/wuU9Hvp5wF7xe/giphy.gif";
+}
+
+function btn2(){
+    img.src = "https://media.giphy.com/media/10H4by255F2UsU/giphy.gif";
+}
+
+function btn3(){
+    img.src = "https://media.giphy.com/media/3o7qE5ceqLBHRR0C64/giphy.gif";
+}
+
+function btn4(){
+    img.src = "https://media.giphy.com/media/myhb94p7RwksU/giphy.gif";
+}
